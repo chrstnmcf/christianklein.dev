01c21c77d7abe33abfe60d50ae9d10262f6d8b6d
Christian Klein <ck.christianklein@gmail.com>
2021-04-13 20:32:25 +0200

    Added group hover for summary cards

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 interface Props {
   href: string;
   target?: string;
+  className?: string;
 }
 
 const CustomLink = (props: React.PropsWithChildren<Props>) => {
--- src/components/Posts/Summary.tsx
+++ src/components/Posts/Summary.tsx
@@ -14,7 +14,7 @@ const Card = tw.span`
   dark:border-gray-700
   text-gray-600
   dark:text-gray-200
-  hover:(text-gray-700 border-white dark:text-gray-300 dark:border-gray-800 shadow-light dark:shadow-dark dark:bg-gray-800)
+  hover:(border-white dark:border-gray-800 shadow-light dark:shadow-dark dark:bg-gray-800)
 `;
 
 export default function Summary({ meta }: Props) {
@@ -23,11 +23,13 @@ export default function Summary({ meta }: Props) {
   const prettyDate = format(new Date(date), 'dd.MM.yyyy');
 
   return (
-    <Link href={`/posts/${slug}`}>
+    <Link href={`/posts/${slug}`} className="group">
       <Card>
-        <h4>{title}</h4>
-        <span tw="text-xs text-gray-400 dark:text-gray-500 mb-2">{prettyDate}</span>
-        <p tw="line-clamp-3 text-sm">{summary}</p>
+        <h4 tw="group-hover:(text-gray-900 dark:text-gray-200)">{title}</h4>
+        <span tw="text-xs mb-2 text-gray-400 dark:text-gray-500 group-hover:(text-gray-400 dark:text-gray-500)">
+          {prettyDate}
+        </span>
+        <p tw="line-clamp-3 text-sm group-hover:(text-gray-900 dark:text-gray-200)">{summary}</p>
       </Card>
     </Link>
   );
